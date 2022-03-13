464584197534ce4b7f486d73ee45cfd6fb914ca3
Sumyeong Kim <admin@sumyeong.kim>
2022-03-13 09:26:46 +0900

    [FEAT] 11055

--- 11055.cpp
+++ 11055.cpp
@@ -1,11 +1,27 @@
 #include <iostream>
-#include <bits/stdc++.h>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    
+    cout.tie(NULL);
+    int N;
+    cin >> N;
+    vector<int> arr(N);
+    vector<int> lis(N);
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i];
+        int maxVal = 0;
+        for (int j = 0; j < i; j++) {
+            if (arr[j] < arr[i]) {
+                maxVal = max(maxVal, lis[j]);
+            }
+        }
+        lis[i] = maxVal + 1;
+    }
+    cout << *max_element(lis.begin(), lis.end());
     return 0;
 }
