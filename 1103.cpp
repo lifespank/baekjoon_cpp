c9f39e9b38a9ee2b64dbe50b1c904ed0ff1b8613
Sumyeong Kim <admin@sumyeong.kim>
2022-03-13 14:36:25 +0900

    [FEAT] 1103

--- 1103.cpp
+++ 1103.cpp
@@ -1,62 +1,65 @@
 #include <cstdio>
 #include <vector>
-#include <queue>
 #include <algorithm>
-#define INF 987654321
 
 using namespace std;
 
 int dx[4] = {1, -1, 0, 0};
 int dy[4] = {0, 0, 1, -1};
-int ballOutMax = 0;
 
-bool isInBoard(int x, int y, int n, int m) {
-    return x >= 0 && x < n && y >= 0 && y < m;
+bool isInBoard(int x, int y, int N, int M) {
+    return x >= 0 && x < N && y >= 0 && y < M;
 }
 
-void dfs(vector<vector<int>> &board, vector<vector<int>> &dp, int x, int y, int depth) {
-    dp[x][y] = max(dp[x][y], depth);
+bool dfs(vector<vector<int>> &board, vector<vector<bool>> &visited, vector<vector<int>> &dp, int hereX, int hereY) {
+    visited[hereX][hereY] = true;
+    if (!board[hereX][hereY]) {
+        visited[hereX][hereY] = false;
+        dp[hereX][hereY] = 0;
+        return true;
+    }
     int maxVal = 0;
     for (int i = 0; i < 4; i++) {
-        int nX = x + dx[i] * board[x][y];
-        int nY = y + dy[i] * board[x][y];
-        if (!isInBoard(nX, nY, board.size(), board[0].size())) {
-            ballOutMax = max(ballOutMax, depth + 1);
-        } else if (board[nX][nY] == 'H' - '0') {
-            dp[nX][nY] = max(dp[nX][nY], depth + 1);
-        } else if (dp[nX][nY] < depth + 1){
-            dfs(board, dp, nX, nY, depth + 1);
-        }
-    }
-}
-
-int main() {
-    int n, m;
-    scanf("%d %d", &n, &m);
-    getchar();
-    vector<vector<int>> board(n, vector<int>(m, 0));
-    vector<vector<int>> dp(n, vector<int>(m, -1));
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            board[i][j] = getchar() - '0';
-        }
-        getchar();
-    }
-    dfs(board, dp, 0, 0, 0);
-    int ans = 0;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            if (board[i][j] == 'H' - '0') {
-                ans = max(ans, dp[i][j]);
-            } else if (dp[i][j] == INF) {
-                ans = max(ans, INF);
+        int nX = hereX + board[hereX][hereY] * dx[i];
+        int nY = hereY + board[hereX][hereY] * dy[i];
+        if (isInBoard(nX, nY, board.size(), board[0].size())) {
+            if (visited[nX][nY]) {
+                return false;
+            } else {
+                if (dp[nX][nY] == -1) {
+                    if (!dfs(board, visited, dp, nX, nY)) {
+                        return false;
+                    }
+                }
+                maxVal = max(maxVal, dp[nX][nY]);
             }
         }
     }
-    ans = max(ans, ballOutMax);
-    if (ans == INF) {
-        ans = -1;
+    dp[hereX][hereY] = maxVal + 1;
+    visited[hereX][hereY] = false;
+    return true;
+}
+
+int main() {
+    int N, M;
+    scanf("%d %d", &N, &M);
+    getchar();
+    vector<vector<int>> board(N, vector<int>(M));
+    vector<vector<bool>> visited(N, vector<bool>(M, false));
+    vector<vector<int>> dp(N, vector<int>(M, -1));
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            board[i][j] = getchar() - '0';
+            if (board[i][j] < 1 || board[i][j] > 9) {
+                board[i][j] = 0;
+            }
+        }
+        getchar();
     }
-    printf("%d", ans);
+    if (!dfs(board, visited, dp, 0, 0)) {
+        printf("%d", -1);
+    } else {
+        printf("%d", dp[0][0]);
+    }
     return 0;
 }
