1b62a85f34c075d2596b0deee14579e3ff72eced
Ubuntu <ubuntu@www.sumyeong.kim>
2022-03-24 19:11:25 +0900

    [FIX] 2042 구간 합 구하기 -  stdio sync 해제

--- 2042.cpp
+++ 2042.cpp
@@ -36,6 +36,9 @@ long long sum(vector<long long> &tree, int node, int low, int high, int left, in
 }
 
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
     int N, M, K;
     cin >> N >> M >> K;
     vector<long long> arr(N);
